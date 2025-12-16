9f91a49587a56ef214310f3c633a953f3da90265
dianebey <diane.beysecker@edu.devinci.fr>
2025-12-16 12:04:04 +0100

    Add files via upload

---
+++ BEYSECKER_DIANE_GRB_javascript/main.js
@@ -0,0 +1,48 @@
+//Exercice noté - Simulateur de combat
+
+let hero = {
+    name = "Bob",
+    pv = 20,
+    attack = "sword strikes",
+    precision = function precise(x) {
+        return x.toPrecision(0.7)
+    }
+}
+
+let ennemi = {
+    name = "Void",
+    pv = 55,
+    attack = "swordStrikes",
+    precision = function precise(x) {
+        return x.toPrecision(0.5)
+    }
+}
+console.log(hero)
+console.log(ennemi)
+
+function combat(hero, ennemi){
+    console.log(`Début du combat entre {hero.name} et {ennemi.name}`.)
+}
+for (let i = 0; i < pv; i++) {
+if (hero.pv > 0 && ennemi.pv > 0) {
+
+   }   
+}
+attack(ennemi)
+    const ennemi.pvNumber
+ennemi.pvNumber = ennemi.pv - attack
+    console.log(`${hero.name} attaque ${ennemi.name}, les pv de l'ennemi sont de ${ennemi.pvNumber}.`)
+precision(ennemi)
+    const ennemi.pvNumber = math.random()
+
+attack(hero)
+    const hero.pvNumber
+    hero.pvNumber = hero.pv - attack
+    console.log(`${ennemi.name} attaque ${hero.name}, les pv du hero sont de ${hero.pvNumber}.`)
+precision(hero)
+    const hero.pvNumber = math.random()
+
+while (condition) {
+    vainqueur
+}
+console.log({vainqueur.name})
